7ef440733bb09b5f281ef65d04f3184a3275db41
Eoin1955 <150018659+Eoin1955@users.noreply.github.com>
2025-04-08 09:51:22 +0100

    Revert "start test files, testing each field"

--- src/main/java/ie/atu/OOPexam2/Patient.java
+++ src/main/java/ie/atu/OOPexam2/Patient.java
@@ -1,19 +1,7 @@
 package ie.atu.OOPexam2;
 
-import java.util.Scanner;
-
 public class Patient {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please Enter name: ");
-        String name = sc.nextLine();
-        System.out.println("Please Enter family name: ");
-        String FamilyName = sc.nextLine();
-        System.out.println("Please enter HSE Number: ");
-        String HSENum = sc.nextLine();
-        System.out.println("Please enter your age: ");
-        int age = sc.nextInt();
-
 
     }
 }
